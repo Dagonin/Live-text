74eae75c77130beacfef9fbec834dc817385e484
Dagonin <dagonins@gmail.com>
2020-01-13 12:55:23 +0100

    pokazywanie pytan i zaznaczanie odpowiedzi

--- models/guest.js
+++ models/guest.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const guestSchema = new mongoose.Schema({
     username: String,
     email: String,
-//    roomquestions: [{
-//        answer: Object,
-//        question: Object
-//    }],
+    roomquestions: [{
+        answer: Object,
+        question: Object
+    }],
     socket: String,
     index: Number,
     time:Number
