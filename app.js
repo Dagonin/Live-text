97e98a4735e59e746212d20da269f4ddd86bb57a
Dagonin <dagonins@gmail.com>
2020-03-10 19:17:41 +0100

    deleted:    My First Project-85925ad950c5.json 	modified:   app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const express = require('express'),
     securePin = require('secure-pin'),
     cookieParser = require('cookie-parser'),
     morgan = require('morgan'),
+    speech = require('@google-cloud/speech'),
     io = require('socket.io')(server, {
         pingTimeout: 5000,
         pingInterval: 25000
@@ -88,6 +89,66 @@ passport.deserializeUser(function (id, done) {
         done(err, user);
     });
 });
+
+
+
+//
+//async function main() {
+//  // Imports the Google Cloud client library
+//
+//  // Creates a client
+//  const client = new speech.SpeechClient();
+//
+//  // The name of the audio file to transcribe
+//  const fileName = './resources/r.mp3';
+//
+//  // Reads a local audio file and converts it to base64
+//  const file = fs.readFileSync(fileName);
+//  const audioBytes = file.toString('base64');
+//
+//  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+//  const audio = {
+//    content: audioBytes,
+//  };
+//  const config = {
+//    encoding: 'LINEAR16',
+//    sampleRateHertz: 16000,
+//    languageCode: 'pl-PL',
+//  };
+//  const request = {
+//    audio: audio,
+//    config: config,
+//  };
+//
+//  // Detects speech in the audio file
+//  const [response] = await client.recognize(request);
+//  const transcription = response.results
+//    .map(result => result.alternatives[0].transcript)
+//    .join('\n');
+//  console.log(`Transcription: ${transcription}`);
+//}
+//main().catch(console.error);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
 
 
 const Guests = require("./models/guest");
